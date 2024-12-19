1d853ddb11499cd378fff713d167b3115a453342
Zaman <zaman.akmyradovic@gmail.com>
2024-12-19 12:46:35 +0500

    added meals grid component

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,5 +1,27 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 const Page = () => {
-  return <div>Meals Page</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default Page;
