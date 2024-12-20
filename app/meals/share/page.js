d500c818f0adcb6fed662adb03e06673b1dfdd5a
Zaman <zaman.akmyradovic@gmail.com>
2024-12-20 14:02:35 +0500

    Adding specific metadata to pages

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -3,11 +3,11 @@
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
-import { useFormStatus } from "react-dom";
+import { useFormState } from "react-dom";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
-  const status = useFormStatus();
+  const [state, formAction] = useFormState(shareMeal, { message: null });
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +17,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -46,6 +46,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p style={{ color: "white" }}>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
